deaad94c4f78e663a90679cc79fd47d207ef9db7
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-02-01 17:38:46 +0200

    start task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const refs = {
+    startBtn: document.querySelector([data - start]),
+    stopBtn: document.querySelector([data - stop])
+};
 
+
+refs.startBtn.addEventListener('click', clickStartBtn);
+refs.stopBtn.addEventListener('click', onClickStopBtn);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function onClickStartBtn {
+    
+};
+
+function onClickStopBtn {
+    
+};
